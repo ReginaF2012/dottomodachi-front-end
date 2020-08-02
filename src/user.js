edb9974a9230e689c659e90be6734ec61335b99c
ReginaF2012 <skritschy@gmail.com>
2020-08-02 17:22:32 -0500

    added logout button and logout functionality

--- src/user.js
+++ src/user.js
@@ -9,6 +9,15 @@ class User {
         let dottomodachiContainer = document.createElement('div')
         dottomodachiContainer.className = 'columns'
         dottomodachiContainer.id = "dottomodachi-container"
+        let logoutArea = document.getElementById('logout-button-area')
+        logoutArea.innerHTML = `
+            <a class="button is-large is-info is-light">
+                <span class="icon">
+                    <i class="fas fa-sign-out-alt"></i>
+                </span>
+                <span>Log Out</span>
+            </a>
+        `
  
         let dottomodachiHeader = `
             <section class="hero is-primary">
@@ -21,7 +30,17 @@ class User {
                 </div>
             </section>
             `
+
         mainSection.innerHTML = dottomodachiHeader
         mainSection.append(dottomodachiContainer)
+
+        logoutArea.addEventListener('click', e => {
+            if (e.target.nodeName === "SPAN"){
+                localStorage.clear()
+                state.page = 'login'
+                logoutArea.innerHTML = ""
+                renderLandingPage()
+            }
+        })
     }
 }
