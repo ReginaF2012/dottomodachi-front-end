0296ba7764b1496d1da7e2015d7bcd927458c669
ReginaF2012 <skritschy@gmail.com>
2020-08-10 18:42:38 -0500

    added extra / to BASEURL

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const BASEURL = "https://dottomodachi-back-end.herokuapp.com/"
+const BASEURL = "https://dottomodachi-back-end.herokuapp.com"
 
 let currentUser
 const userAdapter = new UserAdapter(BASEURL)
